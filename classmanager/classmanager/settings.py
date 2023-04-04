cc4ca9952ed4c84fdef2448d72fac64d336a9107
scavnicka_sarka <scavnicka.sarka@gmail.com>
2023-04-04 22:47:05 +0200

    Updated db driver

--- classmanager/classmanager/settings.py
+++ classmanager/classmanager/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'classmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'homework002admin',
+        'PASSWORD': 'authenticyippeedecember1',
+        'HOST': 'db-homework002.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
